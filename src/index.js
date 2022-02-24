8102f78d8eee53753272af41f11af094523ebd3d
vanisco <vanisco@vanisco-K53SC.vanisco.local>
2022-02-24 13:51:32 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import download from './download.jpeg';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import {books} from './books'
+
+import Book from './Book'
+
+//function Test() {
+  //return (
+    //<div>
+    //<Person />
+    //<Message />
+    //</div>
+  ///);
+//}
+
+//const Person = () =>{
+ //return <h2>Vanisco T</h2>
+//}
+
+//const Message = () =>{
+  //return <h2>Just a Test</h2>
+ //}
+
+//ReactDom.render(<Test />, document.getElementById('root')
+//);
+
+
+function Test() {
+  return (
+    <section className='booklist'>{
+      books.map((book)=>{
+        return (
+          <Book key={book.id} book={book}></Book>
+        );
+    })} 
+    </section>
+  );
+}
+
+ReactDom.render(<Test />, document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
